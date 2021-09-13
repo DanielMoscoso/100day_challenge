cf8ff1e08b8ce83421fb877968bdea8e248d6065
Daniel <daniel_eduardo_4@hotmail.com>
2021-09-13 11:45:48 -0500

    Even better way to write the code. It is simpler.

--- Day_3/warm_up.py
+++ Day_3/warm_up.py
@@ -32,3 +32,23 @@ elif bmi >= 30 and bmi < 35:
     print(f"Your BMI is: {bmi}, are obese.")
 else:
     print(f"Your BMI is: {bmi}, are clinically obese.")
+
+# %%
+# But this is a better written code:
+weight = float(input("What is your weight in Kilograms?: "))
+height = float(input("What is your height in Meters?: "))
+
+bmi = round(weight / height**2)
+
+if bmi < 18.5:
+    print(f"Your BMI is: {bmi}, are underweight.")
+elif bmi < 25:  # If you check for it to be strictly below 18.5, then you know that when it enters this line, it is 18.5+. No need to check it again.
+    print(f"Your BMI is: {bmi}, are normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is: {bmi}, are overweight.")
+elif bmi < 35:
+    print(f"Your BMI is: {bmi}, are obese.")
+else:
+    print(f"Your BMI is: {bmi}, are clinically obese.")
+# %%
+# %%
