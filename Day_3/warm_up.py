3600f62b3fde164ec1310a74f81a334cfd510601
Daniel <daniel_eduardo_4@hotmail.com>
2021-09-13 11:57:26 -0500

    Now, this is and interesting Leap Year calculator.

--- Day_3/warm_up.py
+++ Day_3/warm_up.py
@@ -52,3 +52,13 @@ else:
     print(f"Your BMI is: {bmi}, are clinically obese.")
 # %%
 # %%
+# Leap year Calculator:
+year = int(input("Which year do you want to check?: "))
+
+if year % 4 == 0 and year % 100 != 0:
+    print("It is a leap year!")
+else:
+    if year % 400 == 0:
+        print("It is a leap year!")
+    else:
+        print("It is not a leap year.")
