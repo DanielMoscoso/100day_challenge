2a388b213cbcf7e46e16d279576c3215172a65a8
Daniel <daniel_eduardo_4@hotmail.com>
2021-09-17 15:29:32 -0500

    Different way of seeing the infinite loop problem inside the maze.

--- Day_6/warm_up/Maze_hard/Maze_hard.py
+++ Day_6/warm_up/Maze_hard/Maze_hard.py
@@ -22,11 +22,33 @@ while not at_goal():
         right_token += 1
     elif front_is_clear():
         move()
-    elif not front_is_clear() and not right_is_clear():
+    else:
         turn_left()
         right_token -= 1
+# %%
+# %%
+
+
+# Cool clever, and different way of seeing the infinite loop problem inside the maze:
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+#  This will always make sure that the robot first hits a wall, turns to the left, and starts from there:
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():  # This is the correct way!
+        turn_right()
+        move()  # It turns right AND moves forward in order to get out of the possible infinite loop in the maze.
+    elif front_is_clear():
+        move()
     else:
-        pass
+        turn_left()
 
 ################################################################
 # WARNING: Do not change this comment.
