433bebe7f2dd09cad28346615df9bcab8f7a5a27
Daniel Moscoso <daniel_eduardo_4@hotmail.com>
2021-09-18 16:22:53 -0400

    The professor's way is a different approach. You have to use a boolean to get out of the 'while loop' instead of other conditions. Plus, she used her own modules for printing the pictures, and storing all the words us to guess. She also used the 'join()' method to convert the list into a string; extremely clever. I am going to use it from now on.

--- Day_7/hangman.py
+++ Day_7/hangman.py
@@ -1,12 +1,33 @@
 import random
+import art  # Use this if you want to run it in command line.
+import words_list  # Use this if you want to run it in command line.
+# from Day_7 import art  # Use this if you want to run it in IDE.
+# from Day_7 import words_list  # Use this if you want to run it in IDE.
+import os
+
+# # This is used for debugging purposes.
+# path = os.getcwd()
+# print(path)
 
 # NOTE: The game does not see a repeated answer as a "wrong" answer.
 
+
+# Define our clear function (for when we want to clear the screen after each try):
+def clear():
+    # For windows
+    if os.name == 'nt':
+        os.system('cls')
+
+    # For mac and linux(here, os.name is 'posix')
+    else:
+        os.system('clear')
+
+
 # Initialization:
-words = ["ardvark", "baboon", "camel"]
+# words = ["ardvark", "baboon", "camel"]  # This is used for debugging purposes.
 lives = 6
 
-chosen_word = random.choice(words)
+chosen_word = random.choice(words_list.word_list)
 chosen_word_length = len(chosen_word)
 missing_letters = chosen_word_length
 
@@ -15,12 +36,15 @@ for letters in chosen_word:
     word_list.append("_")
 
 # Instructions:
+print(art.logo)
+# print(f"The word is: {chosen_word}")  # This is used for debugging purposes.
 print(f"lives remaining: {lives}")
-print(f"length of the word: {chosen_word_length}")
-# print(f"missing letters: {missing_letters}\n")  # This is used for debuggin purposes.
+print(f"length of the word: {chosen_word_length}\n\n")
+# print(f"missing letters: {missing_letters}\n")  # This is used for debugging purposes.
 
 while lives > 0 and missing_letters > 0:
     guess = input("Guess a letter: ").lower()
+    clear()
 
     for index, letter in enumerate(chosen_word):
         if letter == guess:
@@ -36,11 +60,110 @@ while lives > 0 and missing_letters > 0:
 
     # If the person did not answer correctly:
     if hangman.count(guess) == 0:
+        print(f'You guessed "{guess}", that is not in the word. You lose a life.')
         lives -= 1
 
     print(hangman)
 
-    print(f"\nmissing letters: {missing_letters}")
+    # This gives a simple GUI of the stick-figure hangman:
+    if lives == 5:
+        print(art.stages[lives])
+    elif lives == 4:
+        print(art.stages[lives])
+    elif lives == 3:
+        print(art.stages[lives])
+    elif lives == 2:
+        print(art.stages[lives])
+    elif lives == 1:
+        print(art.stages[lives])
+    else:
+        print(art.stages[lives])
+
+    if guess in word_list:
+        print(f'You have already guessed "{guess}". Try again.')
+
+    # print(f"\nmissing letters: {missing_letters}")  # This is used for debugging purposes.
     print(f"lives remaining: {lives}\n")
+
+    # This tells the user if they won or lost at the end of the game:
+    if missing_letters == 0 and lives > 0:
+        print("You won!\n")
+    elif missing_letters > 0 and lives == 0:
+        print("You lost!\n")
+    else:
+        pass
+
+print("Game over!")
+# %%
+import random
+import art  # Use this if you want to run it in command line.
+import words_list  # Use this if you want to run it in command line.
+# from Day_7 import art  # Use this if you want to run it in IDE.
+# from Day_7 import words_list  # Use this if you want to run it in IDE.
+import os
+
+# NOTE: The game does not see a repeated answer as a "wrong" answer.
+
+
+# Define our clear function (for when we want to clear the screen after each try):
+def clear():
+    # For windows
+    if os.name == 'nt':
+        os.system('cls')
+
+    # For mac and linux(here, os.name is 'posix')
+    else:
+        os.system('clear')
+
+
+# Initialization:
+# words_list = ["ardvark", "baboon", "camel"]  # This is for debugging
+lives = 6
+
+chosen_word = random.choice(words_list.word_list)
+chosen_word_length = len(chosen_word)
+missing_letters = chosen_word_length
+
+word_list = []
+for letters in chosen_word:
+    word_list.append("_")
+
+print(art.logo)
+
+# Instructions:
+print(f"lives remaining: {lives}")
+print(f"length of the word: {chosen_word_length}")
+# print(f"missing letters: {missing_letters}\n")  # This is used for debugging purposes.
+
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    clear()
+
+    if guess in word_list:
+        print(f'You have already guessed "{guess}". Try again.')
+
+    for position in range(chosen_word_length):
+        letter = chosen_word[position]
+
+        if letter == guess:
+            word_list[position] = letter
+
+    # Merge all the elements in the list and turn it into a String -> "join()", separating the elements with a space.
+    print(f"{' '.join(word_list)}")
+
+    if "_" not in word_list:
+        end_of_game = True
+        print("You win!\n")
+
+    # This gives a simple GUI of the stick-figure hangman:
+    print(art.stages[lives])
+
+    if guess not in chosen_word:
+        print(f'You guessed "{guess}", that is not in the word. You lose a life.')
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose!\n")
 
 print("Game over!")
