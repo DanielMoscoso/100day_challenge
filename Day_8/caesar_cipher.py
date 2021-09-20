56ab0c3416dbd104c29597fa1f23ad5e08b54570
Daniel Moscoso <daniel_eduardo_4@hotmail.com>
2021-09-19 23:39:45 -0400

    The third part is an alternative way of solving the challente. It better uses the 'for loop', shows a different way of using the 'alphabet' list, introduces a little trick for inverting the signs, and better formats the final printed output.

--- Day_8/caesar_cipher.py
+++ Day_8/caesar_cipher.py
@@ -1,4 +1,4 @@
-def encrypt(direction="", text="", shift=0):
+def caesar_cipher(direction="", text="", shift=0):
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     output = ""
@@ -39,4 +39,50 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 # print(encrypt("hello", 5))  # This is for debugging purposes.
 # print(encrypt("civilization", 5))  # This is for debugging purposes.
-print(encrypt(direction, text, shift))
+print(f"The final {direction}d text is: {caesar_cipher(direction, text, shift)}")
+
+
+# %%
+# Alternative:
+def caesar_cipher(direction="", text="", shift=0):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#               The "index()" method will return the first time a letter appears
+#               in the list. So it does not matter how many times it is repeated.
+    output = ""
+
+    for letter in text:
+        original_index = alphabet.index(letter)
+
+        if direction == "encode":
+            # -------------------- This is for encrypting: --------------------
+            # original_index = alphabet.index("v")  # This is for debugging purposes.
+            # original_index  # This is for debugging purposes.
+
+            output += alphabet[original_index + shift]
+            # -------------------- This is for encrypting: --------------------
+        elif direction == "decode":
+            # -------------------- This is for decrypting: --------------------
+            # original_index = alphabet.index("v")  # This is for debugging purposes.
+            # original_index  # This is for debugging purposes.
+
+            output += alphabet[original_index + (shift * -1)]
+        else:
+            pass
+            # -------------------- This is for decrypting: --------------------
+
+    return output
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+# print(encrypt("hello", 5))  # This is for debugging purposes.
+# print(encrypt("civilization", 5))  # This is for debugging purposes.
+
+if direction == "encode" or direction == "decode":
+    print(f"The final {direction}d text is: {caesar_cipher(direction, text, shift)}")
+else:
+    print('I need to "Encrypt" or "Decrypt", try again.\n')
