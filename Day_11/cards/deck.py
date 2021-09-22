ca9eee2ea1c00560c11104f069ddd3f5e85c665a
Daniel Moscoso <daniel_eduardo_4@hotmail.com>
2021-09-22 17:36:45 -0400

    The 'Deck()' class is created, debugges and ready to go. It creates one card of each suit, for a total of 52 cards. Which are instances of their own different classes.

--- Day_11/cards/deck.py
+++ Day_11/cards/deck.py
@@ -4,23 +4,37 @@ from Day_11.cards import king
 from Day_11.cards import queen
 from Day_11.cards import numbers
 
-deck = []
-list_of_suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
-# This is for all the Aces, Jacks, Kings, and Queens cards:
-for specials in list_of_suits:
-    deck.append(ace.Ace(specials))
-    deck.append(jack.Jack(specials))
-    deck.append(king.King(specials))
-    deck.append(queen.Queen(specials))
+class Deck():
+    def __init__(self):
+        self.deck = []
+        self.list_of_suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+        # This is for all the Aces, Jacks, Kings, and Queens cards:
+        for suit in list_of_suits:
+            self.deck.append(ace.Ace(suit))
+            self.deck.append(jack.Jack(suit))
+            self.deck.append(king.King(suit))
+            self.deck.append(queen.Queen(suit))
+
+        # This is for all the numbers:
+        # This will keep track of the suits:
+        for suit in list_of_suits:
+            # This will keep track of the numbers:
+            for number in range(2, 11):  # Remember, 'range()' is not inclusive. [2,11)
+                self.deck.append(numbers.Number(suit, number))
 
 
-# ---- For debugging: ----
-for card in range(4):
-    print(deck[card].name)
-    print("value:", deck[0].value)
-    print(deck[card].suit)
-    print(deck[card].art)
-
-print(f"There are currently: {len(deck)} cards in the deck")
-# ---- For debugging: ----
+# # --------------------------- For debugging: ---------------------------
+# first_deck = deck()
+# # Bulk:
+# for card in range(52):
+#     print("Name: ", first_deck.deck[card].name)
+#     print("Suit:", first_deck.deck[card].suit)
+#     print("Value: ", first_deck.deck[card].value, "\n")
+#
+# print(f"There are currently: {len(first_deck.deck)} cards in the deck")
+#
+# # Individual card:
+# print(first_deck.deck[0].art)
+# # --------------------------- For debugging: ---------------------------
