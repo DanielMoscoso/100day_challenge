9ff6d0223f6f229c4d62667a7433a8dabd266894
Daniel <daniel_eduardo_4@hotmail.com>
2021-10-01 13:39:31 -0500

    Adding the first method.

--- Day_17/warm_up.py
+++ Day_17/warm_up.py
@@ -6,8 +6,19 @@ class User():
         self.user_id = user_id
         self.user_name = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("001", "Daniel")
+user_2 = User("002", "John")
 
-print(user_1.user_name)
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
