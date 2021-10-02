919287b9e45cefd1c3e2d479747edb9ee80d1216
Daniel <daniel_eduardo_4@hotmail.com>
2021-10-02 13:28:06 -0500

    Spirograph 2.0.

--- Day_18/warm_up.py
+++ Day_18/warm_up.py
@@ -68,29 +68,53 @@ tim.color("red")
 # # -------------------------- Completely Random Walk: --------------------------
 
 
-# -------------------------------- Spirograph: --------------------------------
-def random_walk():
-    tim.pensize(1)
-    tim.speed(0)
-    direction = [0, 90, 180, 270]
+# # -------------------------------- Spirograph: --------------------------------
+# def Spirograph():
+#     tim.pensize(1)
+#     tim.speed(0)
+#     direction = [0, 90, 180, 270]
+#
+#     tim.circle(200)
+#
+#     # # This is if you want a completely random color:
+#     # color1 = random.randint(0, 255))
+#     # color2 = random.randint(0, 255))
+#     # color3 = random.randint(0, 255))
+#     # tim.pencolor((color1, color2, color3))
+#
+#     # # This is if you want a set of colors:
+#     colors = ['cyan', 'SeaGreen', 'CornFlowerBlue', 'IndianRed', 'DarkOrchid', 'LightSeaGreen', 'teal', 'slate gray', 'light coral', 'wheat']
+#     tim.color(random.choice(colors))
+#     tim.right(3)
+#
+#
+# for _ in range(120):
+#     Spirograph()
+# # -------------------------------- Spirograph: --------------------------------
 
-    tim.circle(200)
 
-    # # This is if you want a completely random color:
-    # color1 = random.randint(0, 255))
-    # color2 = random.randint(0, 255))
-    # color3 = random.randint(0, 255))
-    # tim.pencolor((color1, color2, color3))
+# ------------------------------ Spirograph 2.0: ------------------------------
+def Spirograph(size_of_gap):
+    for _ in range(360 // size_of_gap):  # '//' means floor division. Normal division would output a float.
+        tim.pensize(1)
+        tim.speed(0)
 
-    # # This is if you want a set of colors:
-    colors = ['cyan', 'SeaGreen', 'CornFlowerBlue', 'IndianRed', 'DarkOrchid', 'LightSeaGreen', 'teal', 'slate gray', 'light coral', 'wheat']
-    tim.color(random.choice(colors))
-    tim.right(3)
+        tim.circle(200)
+
+        # # This is if you want a completely random color:
+        # color1 = random.randint(0, 255))
+        # color2 = random.randint(0, 255))
+        # color3 = random.randint(0, 255))
+        # tim.pencolor((color1, color2, color3))
+
+        # # This is if you want a set of colors:
+        colors = ['cyan', 'SeaGreen', 'CornFlowerBlue', 'IndianRed', 'DarkOrchid', 'LightSeaGreen', 'teal', 'slate gray', 'light coral', 'wheat']
+        tim.color(random.choice(colors))
+        tim.setheading(tim.heading() + size_of_gap)  # This is to also turn right.
 
 
-for _ in range(120):
-    random_walk()
-# -------------------------------- Spirograph: --------------------------------
+Spirograph(5)  # 5 degree ofset from circle to circle.
+# ------------------------------ Spirograph 2.0: ------------------------------
 
 screen = Screen()
 screen.exitonclick()
