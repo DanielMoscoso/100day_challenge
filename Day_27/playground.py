3770e12353de6347c72b507e4ef6b9ccc9635939
Daniel <daniel_eduardo_4@hotmail.com>
2021-10-28 17:51:05 -0500

    class + **kwargs.

--- Day_27/playground.py
+++ Day_27/playground.py
@@ -28,3 +28,16 @@ def calculate(n, **kwargs):
 
 
 calculate(2, add=3, multiply=5)
+
+
+# %%
+# classes and **kwargs:
+class Car:
+    def __init__(self, **kwargs):
+        self.make = kwargs["make"]
+        self.model = kwargs["model"]
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)
+print(my_car.make)
