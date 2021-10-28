255440ccfc187b41421202614cc1eb2909e85e3d
Daniel <daniel_eduardo_4@hotmail.com>
2021-10-28 17:32:16 -0500

    **kwargs

--- Day_27/playground.py
+++ Day_27/playground.py
@@ -1,3 +1,4 @@
+# *args:
 def add(*args):
     token = 0
     for item in args:
@@ -13,3 +14,17 @@ def add2(*args):
 
 add(1, 2, 3, 4)
 add2(1, 2, 3, 5)
+
+
+# %%
+# **kwargs:
+def calculate(n, **kwargs):
+    print(kwargs)
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
