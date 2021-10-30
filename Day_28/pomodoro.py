57db5de002fe7f2013d8f77e5e556a9328f2abff
Daniel <daniel_eduardo_4@hotmail.com>
2021-10-30 18:03:13 -0500

    Background color changed.

--- Day_28/pomodoro.py
+++ Day_28/pomodoro.py
@@ -20,11 +20,12 @@ LONG_BREAK_MIN = 20
 # ------------------------------- UI SETUP ---------------------------------- #
 window = tkinter.Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = tkinter.Canvas(width=200, height=224)
+canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="tomato.png")
-canvas.create_image(102, 112, image=tomato_img)
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
 window.mainloop()
