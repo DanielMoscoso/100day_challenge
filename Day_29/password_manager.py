e0ef4bf4700c243c4144d490198d4bfbcf751521
Daniel <daniel_eduardo_4@hotmail.com>
2021-11-01 12:02:49 -0500

    Lock image.

--- Day_29/password_manager.py
+++ Day_29/password_manager.py
@@ -6,5 +6,13 @@ import tkinter
 
 # ------------------------------- UI SETUP ---------------------------------- #
 window = tkinter.Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg="white")
+
+# Lock image:
+canvas = tkinter.Canvas(width=200, height=200, bg="white", highlightthickness=0)
+lock_img = tkinter.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=0, row=0)
 
 window.mainloop()
