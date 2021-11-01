7dfe2eb84402d0731c7a445d03a88ece73eaf48a
Daniel <daniel_eduardo_4@hotmail.com>
2021-11-01 12:20:22 -0500

    Text boxes.

--- Day_29/password_manager.py
+++ Day_29/password_manager.py
@@ -13,7 +13,7 @@ window.config(padx=20, pady=20)
 canvas = tkinter.Canvas(width=200, height=200, highlightthickness=0)
 lock_img = tkinter.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
-canvas.grid(column=0, row=0)
+canvas.grid(column=1, row=0)
 
 # Labels:
 website = tkinter.Label(text="Website:")
@@ -23,5 +23,14 @@ password = tkinter.Label(text="Password:")
 website.grid(column=0, row=1)
 email_username.grid(column=0, row=2)
 password.grid(column=0, row=3)
+
+# Text boxes:
+website_text = tkinter.Text(height=1, width=35)
+email_username_text = tkinter.Text(height=1, width=35)
+password_text = tkinter.Text(height=1, width=21)
+
+website_text.grid(column=1, row=1, columnspan=2)
+email_username_text.grid(column=1, row=2, columnspan=2)
+password_text.grid(column=1, row=3)
 
 window.mainloop()
