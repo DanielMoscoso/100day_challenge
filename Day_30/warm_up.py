79e4927288c2a51c9392af44397778c8d2e49542
Daniel <danickwilon@gmail.com>
2021-11-02 15:36:59 -0500

    This is how you would most likely solve problem #1.

--- Day_30/warm_up.py
+++ Day_30/warm_up.py
@@ -25,4 +25,6 @@
 try:
     file = open("a_file.txt")
 except FileNotFoundError:  # You can leave it without the actual exception, just plain 'except' and it will still work.
-    print("there was an error!")
+    file = open("a_file.txt", "w")
+    file.write("Something")
+    file.close()
