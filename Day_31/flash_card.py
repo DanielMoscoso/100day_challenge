d3e098933a91b111233b8e6a18c1bf68799a3c35
Daniel <danickwilon@gmail.com>
2021-11-04 16:47:53 -0500

    Random word picked from the cvs file every time a button is clicked.

--- Day_31/flash_card.py
+++ Day_31/flash_card.py
@@ -1,8 +1,27 @@
 import tkinter
 import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-data = pd.read_csv("./data/Eng-Spa_880_frequency_words.csv")
+
+
+# -------------------------------- Random word --------------------------------
+def new_random_word_correct():
+    data = pd.read_csv("./data/Eng-Spa_880_frequency_words.csv")
+    data_dict = data.to_dict(orient="records")
+
+    random_word = random.choice(data_dict)["English"]
+    canvas.itemconfig(word, text=random_word)
+
+
+def new_random_word_wrong():
+    data = pd.read_csv("./data/Eng-Spa_880_frequency_words.csv")
+    data_dict = data.to_dict(orient="records")
+
+    random_word = random.choice(data_dict)["English"]
+    canvas.itemconfig(word, text=random_word)
+
+
 # ------------------------------- UI SETUP ---------------------------------- #
 # Window:
 window = tkinter.Tk()
@@ -14,8 +33,8 @@ canvas = tkinter.Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthi
 # Front of flash card:
 card_front = tkinter.PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_front)
-canvas.create_text(400, 150, text=data.columns[0], font=("Arial", 40, "italic"))
-canvas.create_text(400, 263, text=data["English"][0], font=("Arial", 60, "bold"))
+language = canvas.create_text(400, 150, text=data.columns[0], font=("Arial", 40, "italic"))
+word = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 # # Back of flash card:
 # card_back = tkinter.PhotoImage(file="./images/card_back.png")
@@ -25,8 +44,8 @@ canvas.grid(row=0, column=0, columnspan=2)
 # Buttons:
 image_right = tkinter.PhotoImage(file="./images/right.png")
 image_wrong = tkinter.PhotoImage(file="./images/wrong.png")
-right_button = tkinter.Button(image=image_right, highlightthickness=0)
-wrong_button = tkinter.Button(image=image_wrong, highlightthickness=0)
+right_button = tkinter.Button(image=image_right, highlightthickness=0, command=new_random_word_correct)
+wrong_button = tkinter.Button(image=image_wrong, highlightthickness=0, command=new_random_word_wrong)
 
 wrong_button.grid(row=1, column=0)
 right_button.grid(row=1, column=1)
