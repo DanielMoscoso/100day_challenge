e6039f3022197253bec4c177afe25dc59b36b034
Daniel <danickwilon@gmail.com>
2021-11-06 17:05:40 -0500

    Sending an e-mail.

--- Day_32/birthday_wisher.py
+++ Day_32/birthday_wisher.py
@@ -1,1 +1,12 @@
 import smtplib
+
+my_email = ""
+password = ""
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()  # This is what makes the mail secure (encrypted).
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="",
+                        msg="Subject:Hello\n\nAnd this is the body of the email.")
+    connection.close()
