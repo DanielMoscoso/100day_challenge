967730363077d3a833a3c6cb2efe36fda2ebcdad
Daniel <danickwilon@gmail.com>
2021-11-12 18:27:03 -0500

    API response meaning.

--- Day_33/warm_up.py
+++ Day_33/warm_up.py
@@ -1,4 +1,13 @@
 import requests
 
+"""
+API response meanings:
+    -1xx: Hold on
+    -2xx: Here you go
+    -3xx: Go away (you dont have permission)
+    -4xx: You screwed up (this does not even exist)
+    -5xx: I (the server) screwed up
+"""
+
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 print(response)
